d581d61ca393ae6458448125a96a26db298e8afe
tac3706 <tac3706@gmail.com>
2026-02-14 23:52:02 +0100

    Create printablesData.js

---
+++ Data/printablesData.js
@@ -0,0 +1,20 @@
+export const printables = [
+    {
+        title: "Irregular Verbs Cheat Sheet",
+        type: "PDF",
+        topic: "vocabulary",
+        ageGroup: "teens",
+        teacher: "Travis",
+        url: "../Assets/Resources/irregular_verbs.pdf",
+        tags: ["grammar", "verbs", "printable"]
+    },
+    {
+        title: "Business Email Templates",
+        type: "Document",
+        topic: "business english",
+        ageGroup: "adults",
+        teacher: "Anna",
+        url: "../Assets/Resources/business_emails.docx",
+        tags: ["writing", "business", "formal"]
+    }
+];
