7782fef13e812c799b11d53d355328d16cf35bb6
tac3706 <tac3706@gmail.com>
2026-02-28 21:41:17 +0100

    Update printables-logic.js

--- Scripts/printables-logic.js
+++ Scripts/printables-logic.js
@@ -183,6 +183,11 @@ function applyPrintableFilters() {
                (!langFilter || String(res.language || "").toLowerCase() === langFilter) &&
                (!favOnly || (res.favoritesCount > 0));
     });
+
+    if (favOnly) {
+        filtered.sort((a, b) => (b.favoritesCount || 0) - (a.favoritesCount || 0));
+        }
+
     displayPrintables(filtered);
 }
 
